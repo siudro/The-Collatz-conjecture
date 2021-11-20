529fc65a3a60fe672548dec8916a7f103236f2f4
Sedra <83130573+siudro@users.noreply.github.com>
2021-11-20 18:52:49 +0300

    Update TheCollatzConjecture.cpp

--- TheCollatzConjecture.cpp
+++ TheCollatzConjecture.cpp
@@ -11,60 +11,46 @@
 using namespace std;
 
 int collatz(int num){
+  int count=0;
+  if (num<=0){
+    printf("the  < 0");
+  return 0;
+  }
 
-int count=0;
-if (num<=0)
-{
-printf("the  < 0");
-return 0;
-}
+while(count!=10000){
+  if (num==1){
+    printf("%i",num);
+    return 0;
+  }
+  else if (num%2==0){
+    printf("%i",num);
+    num/=2;
+    printf("  ");
+  }
 
-while(count!=10000)
-
-{
-if (num==1)
-
-{
-printf("%i",num);
-return 0;
-}
-else if (num%2==0)
-{
-
-printf("%i",num);
-num/=2;
-printf("  ");
-}
-
-else 
-{
-printf("%i",num);
-
-num=num*3+1;
-printf("   ");
-
-}
-count++;
+  else{
+    printf("%i",num);
+    num=num*3+1;
+    printf("   ");
+  }
+  count++;
 }
 return 0;
 }
 
-int main()
-{
+int main(){
 
-int num,stat=0;
-cout<<"what num";
-cin>>num;
-pid_t pid;
+  int num,stat=0;
+  cout<<"what num";
+  cin>>num;
+  pid_t pid;
 
-pid=fork();
-if(pid==-1)
-{
-printf("child did not created");
-}
+  pid=fork();
+  if(pid==-1){
+    printf("child did not created");
+  }
 
-else if (pid==0)
-{
+  else if (pid==0){
 printf(":");
 collatz(num);
 
